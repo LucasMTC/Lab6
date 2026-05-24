6fb4f507b1638838688417a9481be4feb8f296d6
LucasMTC <lucas.callisperis@gmail.com>
2026-05-24 17:04:20 -0600

    Addded expanding textbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 function App() {
 
   const geminiApiKey = import.meta.env.VITE_GEMINI_API_KEY
+  const [text, setText] = useState("")
+  const textareaRef = useRef(null)
+
+  const handleChange = (e) => {
+    setText(e.target.value)
+
+    textareaRef.current.style.height = "auto"
+    textareaRef.current.style.height =
+      textareaRef.current.scrollHeight + "px"
+  }
 
   return (
-    <>
-      <h1>Lucas's Chatbot</h1>
-      
-    </>
+    <div id='main-page'>
+      <h1 className='Titulo'>Lucas's Chatbot</h1>
+      <span></span>
+      <h4 className='Titulo'>En que te puedo ayudar el dia de hoy?</h4>
+      <textarea
+        ref={textareaRef}
+        value={text}
+        onChange={handleChange}
+      >
+      </textarea>
+    </div>
   )
 }
 
