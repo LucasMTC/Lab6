522180dc4373fe0a80ef24f70566af3c7a34f471
LucasMTC <lucas.callisperis@gmail.com>
2026-05-24 21:44:01 -0600

    Added API connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState, useRef } from 'react'
+import { GoogleGenAI } from "@google/genai";
 
 function App() {
-
   const geminiApiKey = import.meta.env.VITE_GEMINI_API_KEY
+  const ai = new GoogleGenAI({apiKey: geminiApiKey});
   const [text, setText] = useState("")
   const textareaRef = useRef(null)
+  const textarea = document.getElementById("prompt")
 
   const handleChange = (e) => {
     setText(e.target.value)
@@ -14,17 +16,27 @@ function App() {
       textareaRef.current.scrollHeight + "px"
   }
 
+  async function sumbitRequest() {
+      const response = await ai.models.generateContent({
+        model: "gemini-3.5-flash",
+        contents: textarea.value,
+      });
+      console.log(response.text);
+      textarea.value = ""
+  }
+
   return (
     <div id='main-page'>
       <h1 className='Titulo'>Lucas's Chatbot</h1>
       <span></span>
       <h4 className='Titulo'>En que te puedo ayudar el dia de hoy?</h4>
-      <textarea
+      <textarea id='prompt'
         ref={textareaRef}
         value={text}
         onChange={handleChange}
       >
       </textarea>
+      <button onClick={sumbitRequest}>Send Message</button>
     </div>
   )
 }
