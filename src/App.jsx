cbf2f2dc43ff32421d38925a39936744ea684cae
LucasMTC <lucas.callisperis@gmail.com>
2026-05-24 21:58:46 -0600

    Added memory to the chat to allow for conversations and not one off messages

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,24 @@ import { GoogleGenAI } from "@google/genai";
 function App() {
   const geminiApiKey = import.meta.env.VITE_GEMINI_API_KEY
   const ai = new GoogleGenAI({apiKey: geminiApiKey});
+  const chatRef = useRef(ai.chats.create({model: "gemini-2.5-flash"}))
+  
   const [text, setText] = useState("")
   const textareaRef = useRef(null)
   const textarea = document.getElementById("prompt")
 
   const handleChange = (e) => {
     setText(e.target.value)
-
     textareaRef.current.style.height = "auto"
     textareaRef.current.style.height =
-      textareaRef.current.scrollHeight + "px"
+    textareaRef.current.scrollHeight + "px"
   }
 
   async function sumbitRequest() {
-      const response = await ai.models.generateContent({
-        model: "gemini-3.5-flash",
-        contents: textarea.value,
-      });
-      console.log(response.text);
-      textarea.value = ""
+    const response = await chatRef.current.sendMessage({ message: textarea.value })
+    console.log(response.text);
+    setText("")
+    textareaRef.current.style.height = "auto"
   }
 
   return (
